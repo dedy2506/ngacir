f20911455663a6b5d11298b37105ff64ab7d73be
dedy2506 <51770391+dedy2506@users.noreply.github.com>
2019-06-14 22:06:17 +0700

    Create autobot.sh

---
+++ autobot.sh
@@ -0,0 +1,34 @@
+echo "-WELCOME TO TOOL-
+echo -e '\033[01;32m'
+echo "--------------------------------"
+echo "         TOOL Nyariduwit Onlen        "
+echo "          © author cobàjha     "
+echo "             _Unfaedah_          "
+echo "--------------------------------"
+
+echo -n "Masukkan Nama Daerah Ngepet Kailan  :"
+read Daerah
+echo -n "Masukkan Jumlah Target Uang  :"
+read uang
+echo
+echo -n "Masukkan No Rek Kamu :"
+read norek
+echo -n "Masukkan Nama Rekening  :"
+read nama
+echo
+clear
+echo "load.." | lolcat
+echo "..." | lolcat
+echo "...." | lolcat
+echo "......." | lolcat
+echo "..........." | lolcat
+echo "................. SUCSES/100%" | lolcat
+echo "wait.............." | lolcat 
+clear
+sleep 3
+echo "|=====================================|" | lolcat
+echo "| TRANSAKSI Hasil...                 !|" | lolcat
+echo "|=====================================|" | lolcat
+echo 
+echo "$nama, Uang sejumlah $uang"
+echo "Telah sukses di Transfer ke Nomer Rek Kmu, Silakan Di Cek Kembali" | lolcat 
